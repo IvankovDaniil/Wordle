3e3bfb60cf54c8bff811438aa5cf22c4762fe3c8
Ivankov Daniil <Ivankovdaniil04@gmail.com>
2025-02-06 16:04:18 +0700

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Даниил Иваньков on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
